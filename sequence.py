5043af57c47b7554888ce118ffaabe2338715c29
Jóhann Rúnarsson <johann9898@gmail.com>
2020-09-03 12:27:30 +0000

    Fixed

--- sequence.py
+++ sequence.py
@@ -1,19 +1,18 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-n1 = 1
-n2 = 2
-n3 = 3
-
-print(n1)
-print(n2)
-print(n3)
-for i in range(0,n-3):
-    sum_all = n1 + n2 + n3
-    n1 = n2
-    n2 = n3
-    n3 = sum_all
-        
+n1 = 0
+n2 = 0
+n3 = 1
+for i in range(0,n):
+    if n3 < 2:
+        sum_all = n1 + n2 + n3
+        n2 = n3
+        n3 = sum_all  
+    else:
+        sum_all = n1 + n2 + n3
+        n1 = n2
+        n2 = n3
+        n3 = sum_all
         
     print(sum_all)
-
     
